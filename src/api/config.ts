8a538aae719469cfbe0d1f014d35014dda80bb6e
jonesus <joonas.palosuo@gmail.com>
2021-01-25 21:10:23 +0200

    feat: add build time api url to env

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,3 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL =
+  typeof window === 'undefined' ? process.env.BUILD_API_URL : process.env.NEXT_PUBLIC_API_URL;
 export const ASSET_URL = process.env.NEXT_PUBLIC_ASSET_URL;
