fa7885ea977dbfe1274f540a6a87ca6266739a70
jonesus <joonas.palosuo@gmail.com>
2021-01-25 23:26:56 +0200

    feat: add serverside asset url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,4 @@
 export const API_URL =
-  typeof window === 'undefined' ? process.env.BUILD_API_URL : process.env.NEXT_PUBLIC_API_URL;
-export const ASSET_URL = process.env.NEXT_PUBLIC_ASSET_URL;
+  typeof window === 'undefined' ? process.env.SERVER_API_URL : process.env.NEXT_PUBLIC_API_URL;
+export const ASSET_URL =
+  typeof window === 'undefined' ? process.env.SERVER_ASSET_URL : process.env.NEXT_PUBLIC_ASSET_URL;
