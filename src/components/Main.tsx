291e21efbffc4c119f9d5636b05a7a45bd8b3a9f
jonesus <joonas.palosuo@gmail.com>
2021-01-29 22:55:20 +0200

    fix: server and client prop mismatch

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,23 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 export const Main: React.FC = (props) => {
-  let navbarHeight = 110;
-  if (typeof document !== 'undefined') {
-    const header = document.body.querySelector('header');
-    if (header) {
-      navbarHeight = header.offsetHeight;
+  const navbarHeight = useRef(110);
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      const header = document.body.querySelector('header');
+      if (header) {
+        navbarHeight.current = header.offsetHeight;
+      }
     }
-  }
+  }, [navbarHeight]);
 
   return (
     <StyledMain
       {...props}
       style={
         {
-          '--navbar-height': `${navbarHeight}px`,
+          '--navbar-height': `${navbarHeight.current}px`,
         } as React.CSSProperties
       }
     />
