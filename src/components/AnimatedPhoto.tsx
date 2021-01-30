008b7fbc1fcafe4837d4daf687d73bb9d2dfe436
jonesus <joonas.palosuo@gmail.com>
2021-01-30 19:34:45 +0200

    feat: skip staggering for photos outside viewport

--- src/components/AnimatedPhoto.tsx
+++ src/components/AnimatedPhoto.tsx
@@ -1,13 +1,33 @@
 import Image, { ImageProps } from 'next/image';
-import { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useCallback, useState } from 'react';
 import styled from 'styled-components';
 
 type AnimatedPhotoProps = ImageProps & {
   staggerIndex?: number;
 };
 
+function isElementInViewport(rect: DOMRect | null) {
+  if (typeof window !== 'undefined' && typeof document !== 'undefined' && rect !== null) {
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  } else {
+    return true;
+  }
+}
+
 export const AnimatedPhoto: React.FC<AnimatedPhotoProps> = ({ staggerIndex, ...props }) => {
   const [visible, setVisible] = useState(false);
+  const [wrapperRect, setWrapperRect] = useState<DOMRect | null>(null);
+  const wrapperRef = useCallback((node: HTMLDivElement) => {
+    if (node !== null) {
+      setWrapperRect(node.getBoundingClientRect());
+    }
+  }, []);
+
   // Hack for next/image onload issue
   // https://github.com/vercel/next.js/issues/20368#issuecomment-757446007
   const onImageLoad = (e: SyntheticEvent<HTMLImageElement>) => {
@@ -15,7 +35,12 @@ export const AnimatedPhoto: React.FC<AnimatedPhotoProps> = ({ staggerIndex, ...p
   };
 
   return (
-    <TransitionWrapper visible={visible} staggerIndex={staggerIndex}>
+    <TransitionWrapper
+      ref={wrapperRef}
+      visible={visible}
+      staggerIndex={staggerIndex}
+      skipStagger={!isElementInViewport(wrapperRect)}
+    >
       <Image {...props} onLoad={onImageLoad} />;
     </TransitionWrapper>
   );
@@ -24,12 +49,13 @@ export const AnimatedPhoto: React.FC<AnimatedPhotoProps> = ({ staggerIndex, ...p
 const TransitionWrapper = styled.div<{
   visible: boolean;
   staggerIndex?: number;
+  skipStagger: boolean;
 }>`
   height: 100%;
   transition-property: transform, opacity;
   transition-duration: 1s, 0.7s;
   transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1), ease-in-out;
-  transition-delay: calc(${(p) => p.staggerIndex || 0} * 100ms);
+  transition-delay: calc(${(p) => p.staggerIndex || 0} * 150ms);
 
   --shift-amount: 2rem;
   --opacity: 0;
@@ -44,4 +70,6 @@ const TransitionWrapper = styled.div<{
   --opacity: 1;
   `
       : ''}
+
+  ${(p) => (p.skipStagger ? 'transition-delay: 0ms' : '')}
 `;
