523141173e7e14443a131a18372b3ed98257b851
jonesus <joonas.palosuo@gmail.com>
2021-01-31 12:14:33 +0200

    feat: embed styles in head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,26 @@ import { getPageBySlug } from 'api';
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+const fontStyles = `
+@font-face {
+  font-family: 'Raleway';
+  src: url('/fonts/Raleway-Italic-VariableFont_wght-subset.woff2') format('woff2');
+  font-weight: 1 999;
+  font-style: italic;
+  font-display: swap;
+  unicode-range: U+20-7E, U+C4, U+C5, U+D6, U+E4, U+E5, U+F6;
+}
+
+@font-face {
+  font-family: 'Raleway';
+  src: url('/fonts/Raleway-VariableFont_wght-subset.woff2') format('woff2');
+  font-weight: 1 999;
+  font-style: normal;
+  font-display: swap;
+  unicode-range: U+20-7E, U+C4, U+C5, U+D6, U+E4, U+E5, U+F6;
+}
+`;
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
@@ -49,7 +69,7 @@ export default class MyDocument extends Document {
             type="font/woff2"
             crossOrigin="anonymous"
           />
-          <link rel="stylesheet" href="/styles.css" />
+          <style dangerouslySetInnerHTML={{ __html: fontStyles }} />
         </Head>
         <body>
           <Main />
